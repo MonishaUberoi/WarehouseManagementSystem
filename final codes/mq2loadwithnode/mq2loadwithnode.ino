2fe28ecc136e9280e7d0998239d0598d6eb3555e
Jimitpatel1729 <jimitmpatel68@gmail.com>
2022-03-25 23:17:34 +0530

    added date in mq2 code

--- final codes/mq2loadwithnode/mq2loadwithnode.ino
+++ final codes/mq2loadwithnode/mq2loadwithnode.ino
@@ -7,18 +7,18 @@
 
 #define FIREBASE_HOST "warehouse-d9de2-default-rtdb.europe-west1.firebasedatabase.app"
 #define FIREBASE_AUTH "rYNBFxHhOXZjQxl41cY7JLVcRVA4OxTvKqmmUHYp"
-#define WIFI_SSID "Virus"
-#define WIFI_PASSWORD "3833207173"
+#define WIFI_SSID "PatelsWifi4G"
+#define WIFI_PASSWORD "9820769386"
 
 //MQ2
 int smokeA0 = A0;
 int sensorThres = 50;
-int n=0;
+int n = 0;
 
 //Load cell
 WiFiClient client;
-HX711 scale(D4, D5);
- //d4 sck, d5 dt
+HX711 scale;
+//d5 sck, d6 dt
 float weight;
 float calibration_factor = -467300; // for me this value works just perfect 419640
 // -107325
@@ -26,13 +26,15 @@ float calibration_factor = -467300; // for me this value works just perfect 4196
 //NTP
 const long utcOffsetInSeconds = 19800;
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+String months[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
-void setup(){
+void setup() {
   pinMode(smokeA0, INPUT);
   Serial.begin(115200);
+  scale.begin(D5, D6);
   scale.set_scale();
   scale.tare(); //Reset the scale to 0
   long zero_factor = scale.read_average(); //Get a baseline reading
@@ -47,7 +49,7 @@ void setup(){
   Serial.println();
   Serial.print("connected: ");
   Serial.println(WiFi.localIP());
-  
+
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   timeClient.begin();
 }
@@ -58,14 +60,20 @@ void loop() {
 
   //MQ2
   int analogSensor = analogRead(smokeA0);
-  Serial.println(analogSensor); 
-  if (analogSensor > sensorThres){
+  Serial.println(analogSensor);
+  if (analogSensor > sensorThres) {
     Serial.print("Smoke detected with concentration of :");
     Serial.print(analogSensor);
     Serial.println("ppm");
     // append a new value to /sensorValue
-    String data = String(daysOfTheWeek[timeClient.getDay()]) + " " + String(timeClient.getFormattedTime()) + " " + String(analogSensor);
-    String name = Firebase.pushString("MQ2Sensor", data);
+    timeClient.update();
+    unsigned long epochTime = timeClient.getEpochTime();
+    struct tm *ptm = gmtime ((time_t *)&epochTime);
+    String currentDate = String(ptm->tm_mday) + "/" + String(ptm->tm_mon + 1) + "/" + String(ptm->tm_year + 1900);
+
+    //pushing and printing
+    String content = currentDate + " " + String(daysOfTheWeek[timeClient.getDay()]) + " " + String(timeClient.getFormattedTime()) + " " + String(analogSensor);
+    String name = Firebase.pushString("MQ2Sensor", content);
   }
   // handle error
   if (Firebase.failed()) {
@@ -79,16 +87,15 @@ void loop() {
   //Load Cell
   scale.set_scale(calibration_factor); //Adjust to this calibration factor
   weight = scale.get_units(5);
-  float noOfUnits = weight/0.175;
-  if(noOfUnits - int(noOfUnits) < 0.5){
+  float noOfUnits = weight / 0.175;
+  if (noOfUnits - int(noOfUnits) < 0.5) {
     noOfUnits = int(noOfUnits);
-  }else{
-    noOfUnits = int(noOfUnits)+1;
+  } else {
+    noOfUnits = int(noOfUnits) + 1;
   }
   Serial.print("No of units left: ");
   Serial.println(noOfUnits);
-  String data = String(daysOfTheWeek[timeClient.getDay()]) + " " + String(timeClient.getFormattedTime()) + " " + String(noOfUnits);
-  String name = Firebase.pushString("Inventory/Nirma Soap", data);
+  String name = Firebase.setFloat("Inventory/Nirma Soap", noOfUnits);
   // handle error
   if (Firebase.failed()) {
     Serial.println(Firebase.error());
@@ -97,6 +104,6 @@ void loop() {
   }
   Serial.print("pushed: /Inventory/Nirma Soap : ");
   Serial.println(data);
-  
+
   delay(500);
 }
